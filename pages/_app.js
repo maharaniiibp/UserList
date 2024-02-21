5bc5481d9bdda5cb568e415bdce0e45c9fff2ead
Maharaniiibp <maharanibanicaputri@gmail.com>
2024-02-21 14:49:00 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <style>
+        {`
+            body {
+              background-color: #171823;
+              margin: 0;
+              padding: 0;
+            }
+          `}
+      </style>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
